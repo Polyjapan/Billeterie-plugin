e889808406ef30270a6798ac4e11a61043119b6f
Tony Clavien <tony.clavien@epfl.ch>
2015-09-28 06:33:36 +0200

    Working on plugin settings

--- Plugin/BasicTicketting/db/database.php
+++ Plugin/BasicTicketting/db/database.php
@@ -1,14 +1,5 @@
 <?php
-/*
-Plugin Name: BasicTicketting
-Plugin URI: 
-Description: Management of the ticketing system used in Japan Impact
-Version: 0.1
-Author: Fabien Mottier, Tony Clavien
-Author URI: www.japan-impact.ch
-License: GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-*/
+
 
 /**
 *	The class used for the installation and desinstallation of the db
@@ -28,54 +19,11 @@ class BaTi_Install_Plugin
     {
 		// db variable
 		global $wpdb;
-		
-		if($wpdb->query($this->BaTi_get_sql_create_client()) === false)
-		{
-			trigger_error("DB ERROR : error when creating table client",E_ERROR);
-		}
-		
-		if($wpdb->query($this->BaTi_get_sql_create_TypeBillet()) === false)
-		{
-			trigger_error("DB ERROR : error when creating table TypeBillet",E_ERROR);
-		}
-		
-		if($wpdb->query($this->BaTi_get_sql_create_Event()) === false)
-		{
-			trigger_error("DB ERROR : error when creating table Event",E_ERROR);
-		}
-		
-		if($wpdb->query($this->BaTi_get_sql_create_Origine()) === false)
-		{
-			trigger_error("DB ERROR : error when creating table Origine",E_ERROR);
-		}
-		
-		if($wpdb->query($this->BaTi_get_sql_create_Commande()) === false)
-		{
-			trigger_error("DB ERROR : error when creating table Commande",E_ERROR);
-		}
-		
-		if($wpdb->query($this->BaTi_get_sql_create_Billet()) === false)
-		{
-			trigger_error("DB ERROR : error when creating table Billet",E_ERROR);
-		}
-		
-        
-    }   // END BaTi_install_DB
-	
-	/**
-	*	Return the sql query to create the table client
-	*		This table will contain the informations about the client when he will buy a ticket
-	*/
-	private static function BaTi_get_sql_create_client()
-	{
-		// db variable
-        global $wpdb;
-		
 		// charset
 		$charset_collate = $wpdb->get_charset_collate();
-
-        // table client
-        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblClient (
+		
+		//This table will contain the informations about the client when he will buy a ticket
+		if($wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblClient (
                     PKClient INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     cliNom VARCHAR(45) NOT NULL,
                     cliPrenom VARCHAR(45) NOT NULL,
@@ -83,25 +31,12 @@ class BaTi_Install_Plugin
                     cliMail VARCHAR(45) NOT NULL,
 
                     CONSTRAINT UNIQUE Unique_Password (cliMail)
-                ) $charset_collate;";
-				
-		return $sql;
-	}
-	
-	/**
-	*	Create the sql query to create the table TypeBillet
-	*		This table will hold the different types of a tickets ( 1 or 2 days and so on )
-	*/
-	private static function BaTi_get_sql_create_TypeBillet() 
-	{
-		// db variable
-        global $wpdb;
-		
-		// charset
-		$charset_collate = $wpdb->get_charset_collate();
-
-        // table TypeBillet query
-        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblTypeBillet (
+                ) $charset_collate;") === false)
+		{
+			trigger_error("DB ERROR : error when creating table client",E_ERROR);
+		}
+		// This table will hold the different types of a tickets ( 1 or 2 days and so on )
+		if($wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblTypeBillet (
                     PKTypeBillet INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     tybiNom VARCHAR(45) NOT NULL,
                     tybiPrix DOUBLE NOT NULL,
@@ -109,71 +44,33 @@ class BaTi_Install_Plugin
 					
 
                     CONSTRAINT UNIQUE Unique_Nom (tybiNom)
-                ) $charset_collate;";
-				
-		return $sql;
-	}
-	
-	/**
-	*	Create the sql query to create the table Event
-	*			Holds the name of the event
-	*/
-	private static function BaTi_get_sql_create_Event() 
-	{
-		// db variable
-        global $wpdb;
-		
-		// charset
-		$charset_collate = $wpdb->get_charset_collate();
-
-        // table Event query
-        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblEvent (
+                ) $charset_collate;") === false)
+		{
+			trigger_error("DB ERROR : error when creating table TypeBillet",E_ERROR);
+		}
+		// Holds the name of the event
+		if($wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblEvent (
                     PKEvent INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     eveNom VARCHAR(45) NOT NULL,
 
 
                     CONSTRAINT UNIQUE Unique_Nom (eveNom)
-                ) $charset_collate;";
-		return $sql;
-	}
-	
-	/**
-	*	Create the sql query to create the table Origine
-	*		Holds the origin of the tickets
-	*/
-	private static function BaTi_get_sql_create_Origine() 
-	{
-		// db variable
-        global $wpdb;
-		
-		// charset
-		$charset_collate = $wpdb->get_charset_collate();
-
-        // table Origine query
-        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblOrigine (
+                ) $charset_collate;") === false)
+		{
+			trigger_error("DB ERROR : error when creating table Event",E_ERROR);
+		}
+		// Holds the origin of the tickets
+		if($wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblOrigine (
                     PKOrigine INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     oriNom VARCHAR(45) NOT NULL,
 
                     CONSTRAINT UNIQUE Unique_Nom (oriNom)
-                ) $charset_collate;";
-				
-		return $sql;
-	}
-	
-	/**
-	*	Create the sql query to create the table Commande
-	*		Holds all the transaction of the client
-	*/
-	private static function BaTi_get_sql_create_Commande() 
-	{
-		// db variable
-        global $wpdb;
-		
-		// charset
-		$charset_collate = $wpdb->get_charset_collate();
-
-        // table Commande query
-        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblCommande (
+                ) $charset_collate;") === false)
+		{
+			trigger_error("DB ERROR : error when creating table Origine",E_ERROR);
+		}
+		// Holds all the transaction of the client
+		if($wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblCommande (
                     PKCommande INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     FKClient INT NOT NULL,
                     comDate DATE NOT NULL,
@@ -181,31 +78,21 @@ class BaTi_Install_Plugin
 					comValidate BOOLEAN NOT NULL,
 					CONSTRAINT FK_Commande_Client FOREIGN KEY (FKClient)
 					REFERENCES {$wpdb->prefix}BaTi_tblClient(PKClient)
-                )  $charset_collate;";
-		return $sql;
-	}
-	
-	/**
-	*	Create the sql query to create the table Billet
-	*		The successful generated tickets
-	*/
-	private static function BaTi_get_sql_create_Billet() 
-	{
-		// db variable
-        global $wpdb;
+                )  $charset_collate;") === false)
+		{
+			trigger_error("DB ERROR : error when creating table Commande",E_ERROR);
+		}
 		
-		// charset
-		$charset_collate = $wpdb->get_charset_collate();
 
-        // table Billet query
-        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblBillet (
+		// 	The successful generated tickets
+		if($wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}BaTi_tblBillet (
                     PKBillet INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                     FKCommande INT NOT NULL,
                     FKTypeBillet INT NOT NULL,
                     FKEvent INT NOT NULL,
 					FKOrigine INT NOT NULL,
                     bilCodeBarre INT NOT NULL,
-                    bilEnterDate DATETIME),
+                    bilEnterDate DATETIME,
                     CONSTRAINT UNIQUE Unique_CodeBarre (bilCodeBarre),
 					CONSTRAINT FK_Billet_Commande FOREIGN KEY (FKCommande)
 					REFERENCES {$wpdb->prefix}BaTi_tblCommande(PKCommande),
@@ -215,10 +102,16 @@ class BaTi_Install_Plugin
 					REFERENCES {$wpdb->prefix}BaTi_tblEvent(PKEvent),
 					CONSTRAINT FK_BILLET_Origine FOREIGN KEY (FKOrigine)
 					REFERENCES {$wpdb->prefix}BaTi_tblOrigine(PKOrigine)
-                ) $charset_collate;";
-				
-		return $sql;
-	}
+                ) $charset_collate;") === false)
+		{
+			trigger_error("DB ERROR : error when creating table Billet",E_ERROR);
+		}
+		
+        
+    }   // END BaTi_install_DB
+	
+
+	
 
     public static function BaTi_uninstall_DB()
     {
@@ -228,12 +121,12 @@ class BaTi_Install_Plugin
         global $wpdb;
 
         // Suppression de tables
-        $wpdb->query("DELETE {$wpdb->prefix}BaTi_tblBillet;");
-        $wpdb->query("DELETE {$wpdb->prefix}BaTi_tblCommande;");
-        $wpdb->query("DELETE {$wpdb->prefix}BaTi_tblEvent;");
-		$wpdb->query("DELETE {$wpdb->prefix}BaTi_tblOrigine;");
-        $wpdb->query("DELETE {$wpdb->prefix}BaTi_tblTypeBillet;");
-        $wpdb->query("DELETE {$wpdb->prefix}BaTi_tblClient;");
+        $wpdb->query("DROP TABLE {$wpdb->prefix}BaTi_tblBillet;");
+        $wpdb->query("DROP TABLE {$wpdb->prefix}BaTi_tblCommande;");
+        $wpdb->query("DROP TABLE {$wpdb->prefix}BaTi_tblEvent;");
+		$wpdb->query("DROP TABLE {$wpdb->prefix}BaTi_tblOrigine;");
+        $wpdb->query("DROP TABLE {$wpdb->prefix}BaTi_tblTypeBillet;");
+        $wpdb->query("DROP TABLE {$wpdb->prefix}BaTi_tblClient;");
         
         $Options = $wpdb->get_col("SELECT option_name FROM {$wpdb->prefix}options WHERE option_name LIKE 'BaTi_%';");
 
--- Plugin/BasicTicketting/menus/admin-menu.php
+++ Plugin/BasicTicketting/menus/admin-menu.php
@@ -1,4 +1,6 @@
 <?php
+
+
 /**
 		admin menu building code
 	*/
@@ -8,6 +10,9 @@
 		add_menu_page('Homepage', 'BasicTicketting', 'manage_options', 'BaTi_HomePage', 'BaTi_Admin_HomePage');
 		/** Adding a sub menu */
 		add_submenu_page('BaTi_HomePage', 'Manage', 'Gestion', 'manage_options', 'BaTi_Manage', 'BaTi_Admin_Manage');
+		
+		//call register settings function
+		add_action( 'admin_init', 'register_settings' );
 	}
 	
 	/**
@@ -15,6 +20,11 @@
 	*/
 	function BaTi_Admin_HomePage()
 	{
+		//must check that the user has the required capability 
+		if (!current_user_can('manage_options'))
+		{
+		  wp_die( __('You do not have sufficient permissions to access this page.') );
+		}
 		echo '<div>
 			<h1>
 				Admin Home Page
@@ -23,13 +33,198 @@
 	}
 	
 	/**
-	*
+	* Sub menu
 	*/
 	function BaTi_Admin_Manage() 
 	{
-		echo '<div>
-			<h1>
-				Manage Page
-			</h1>
-		</div>';
+		$TBillet = "BaTi_tblTypeBillet";
+		$TEvents = "BaTi_tblEvent";
+		$TOrigine = "BaTi_tblOrigine";
+		
+		
+		
+	?>
+		
+			<div class="wrap-selection">
+				<h2> BasicTicketting Management</h2>
+				<form method="post" action="#">
+					<select name="BaTi_choice">
+						<option value="<?php echo $TBillet; ?>"><?php echo $TBillet; ?></option>
+						<option value="<?php echo $TEvents; ?>"><?php echo $TEvents; ?></option>
+						<option value="<?php echo $TOrigine; ?>"><?php echo $TOrigine; ?></option>
+					</select>
+					<input type="submit" class="BaTi_Admin_submit" name="BaTi_Choice_Sender" value="Selectionner" />
+				</form>
+			</div>
+			</hr>
+		<?php
+		
+		if (isset($_POST["BaTi_Choice_Sender"])) // if a value was selected
+		{
+			BaTi_Create_Form($_POST["BaTi_choice"]);
+		/*
+			switch($_POST["BaTi_choice"]) // we show the corresponding menu
+			{
+				case $TBillet:
+					BaTi_managing_TypeBillet();
+					break;
+				case $TEvents:
+					BaTi_managing_Events();
+					break;
+				case $TOrigine:
+					BaTi_managing_Origine();
+					break;
+			}*/
+		}
+		/* Get stuff ----------------------------------*/
+		if(isset($_GET["src"]))
+		{
+			switch($_GET["src"])
+			{
+				case "Origine" :
+					echo 'a';
+					break;
+				case "Events" :
+					echo 'b';
+					break;
+				case "TypeBillet" :
+					echo 'c';
+					break;
+			}
+		}
+					
+		
+		/* Origin adding stuff --------------------------------------------------*/
+		if (isset($_POST["Bati_New_Origine"])) // if a value from Origin management is added
+		{
+			global $wpdb;
+			$wpdb->insert($wpdb->prefix.'BaTi_tblOrigine',array('oriNom' => $_POST['BaTi_New_Origine_Name']));
+			BaTi_managing_Origine();
+		}
+		
+		
+		
+		
+	}
+	
+	function BaTi_managing_TypeBillet()
+	{
+		?>
+			<h2>Gestion des Types de Billet</h2>
+		<?php
+	}
+	function BaTi_managing_Events()
+	{
+		?>
+			<h2>Gestion des Events</h2>
+		<?php
+	}
+	function BaTi_managing_Origine()
+	{
+		?>
+			<h2>Gestion des Origines</h2>
+			<form method="post" action="#">
+				<table>
+					<tr>
+						<th>ID</th>
+						<th>Origine</th>
+						<th></th>
+					</tr>
+
+		<?php
+		// get the elements of the table
+		global $wpdb;
+		$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}BaTi_tblOrigine");
+		foreach($results as $row)
+		{
+			echo "<tr>";
+				echo "<td>";
+					echo $row->PKOrigine;
+				echo "</td>";
+				echo "<td>";
+					echo '<input type="text" name="BaTi_Update_Origine_Text" value="'.$row->oriNom.'">';
+				echo "</td>";
+				echo "<td>";
+					echo '<a href=#?src=Origine&action=update&item='.$row->PKOrigine.'&value='.$row->oriNom.'>update</a>';
+				echo "</td>";
+				echo "<td>";
+					echo '<a href=#?src=Origine&action=delete&item='.$row->PKOrigine.'>delete</a>';
+				echo "</td>";
+			echo "</tr>";
+		}
+		?>
+					<tr>
+					<td></td>
+					<td><input type="text" name="BaTi_New_Origine_Name"></td>
+					<td><input type="submit" value="ajouter" name="Bati_New_Origine" ></td>
+					</table>
+			</form>
+		<?php
+	}
+	
+	function BaTi_Create_Form($tablename)
+	{
+		?>
+		<h2>Gestion de <?php echo $tablename; ?></h2>
+			<form method="post" action="#">
+				<table>
+				<tr>
+		<?php
+		// get all the elements of the table
+		global $wpdb;
+		$results = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}".$tablename);
+		$header = array_keys(get_object_vars ($results['0']));
+		$columns = count($header);
+		foreach($header as $head)
+		{
+			echo "<th>".$head."</th>";
+		}
+		echo "</tr>";
+		foreach($results as $object)
+		{
+			$row = get_object_vars($object);
+			$first = true;
+			$id = -1;
+			foreach($header as $head)
+			{
+				if($first)
+				{
+					$first = false;
+					$id = $row[$head];
+					echo "<td>";
+						echo $id;
+					echo "</td>";
+				}
+				else
+				{
+					echo "<td>";
+						echo '<input type="text" name="BaTi_Update_'.$tablename.'_Text" value="'.$row[$head].'">';
+					echo "</td>";
+				}
+			}
+			echo "<td>";
+					echo '<a href="#?src='.$tablename.'&action=update&item='.$id.'">update</a>';
+				echo "</td>";
+				echo "<td>";
+					echo '<a href="#?src='.$tablename.'&action=delete&item='.$id.'">delete</a>';
+				echo "</td>";
+			echo "</tr>";
+		}
+		echo "<tr>";
+			echo "<td>";
+			echo "</td>";
+			for ($i = 2; $i <= $columns; $i++) {
+				echo "<td>";
+					echo '<input type="text" name="BaTi_New_'.$tablename.'_Text">';
+				echo "</td>";
+			}
+			echo '<td><input type="submit" value="ajouter" name="Bati_New_Origine" ></td>';
+		echo "</tr>";
+		echo "</table>";
+	}
+	
+	function register_settings() {
+		register_setting('myoptions','BaTi_Type_Billet');
+		register_setting('myoptions','BaTi_Events');
+		register_setting('myoptions','BaTi_Origines');
 	}
